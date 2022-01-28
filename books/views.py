63dcbc099c4f7bb901dbfe5e3334f3a1bf7d57ba
Asliddin <asliddintukhtasinov5@gmail.com>
2022-01-29 00:21:44 +0500

    12-change book's views to  Generic View(ListView, DetailView)

--- books/views.py
+++ books/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView
 
 from books.models import Book
 
 
-class BooksView(View):
+class BooksView(ListView):
     template_name = "books/books_list.html"
-
-    def get(self, request):
-        books = Book.objects.all()
-        return render(request=request, template_name=self.template_name, context={"books": books})
+    queryset = Book.objects.all()
+    context_object_name = "books"
 
 
-class BookDetailView(View):
+# class BooksView(View):
+#     template_name = "books/books_list.html"
+#
+#     def get(self, request):
+#         books = Book.objects.all()
+#         return render(request=request, template_name=self.template_name, context={"books": books})
+
+
+class BookDetailView(DetailView):
     template_name = "books/book_detail.html"
+    pk_url_kwarg = "id"
+    model = Book
+    context_object_name = 'book'
 
-    def get(self, request, id):
-        book = Book.objects.get(id=id)
-        return render(request, self.template_name, {"book": book})
+
+# class BookDetailView(View):
+#     template_name = "books/book_detail.html"
+#
+#     def get(self, request, id):
+#         book = Book.objects.get(id=id)
+#         return render(request, self.template_name, {"book": book})
