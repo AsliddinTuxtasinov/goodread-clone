948a07eb7288ea399ef13eeebdce276c2abe6a78
Asliddin <69431423+AsliddinTuxtasinov@users.noreply.github.com>
2022-03-19 19:15:42 +0500

    Update settings.py

--- goodread/settings.py
+++ goodread/settings.py
@@ -136,8 +136,8 @@ LOGIN_URL = "auusers:login"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "asliddintukhtasinov5@gmail.com"
-EMAIL_HOST_PASSWORD = "asliddin5@gmail.com"
+EMAIL_HOST_USER = "example@gmail.com"
+EMAIL_HOST_PASSWORD = "123456"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
